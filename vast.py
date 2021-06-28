07abd561c0647b5bf8cc6d4a2f38f5a3ac986626
Etherion <19214485+jjziets@users.noreply.github.com>
2021-06-28 12:13:38 +0200

    Update vast.py
    added min-chunk to vast.py for setting minim gpu

--- vast.py
+++ vast.py
@@ -534,14 +534,15 @@ def show__machines(args):
     argument("-s", "--price_disk",  help="storage price in $/GB/month (price for inactive instances), default: $0.15/GB/month", type=float),
     argument("-u", "--price_inetu", help="price for internet upload bandwidth in $/GB", type=float),
     argument("-d", "--price_inetd", help="price for internet download bandwidth in $/GB", type=float),
-    usage = "vast list machine id [--price_gpu PRICE_GPU] [--price_inetu PRICE_INETU] [--price_inetd PRICE_INETD] [--api-key API_KEY]",
+    argument("-m", "--min_chunk", help="minimum amount of gpus", type=int),  
+  usage = "vast list machine id [--price_gpu PRICE_GPU] [--price_inetu PRICE_INETU] [--price_inetd PRICE_INETD] [--api-key API_KEY]",
 )
 def list__machine(args):
     #req_url = args.url + "/machines/create_asks/?user_id=" + str(args.user);
     req_url = apiurl(args, "/machines/create_asks/");
 
     #print("PUT " + req_url);
-    r = requests.put(req_url, json = {'machine':args.id, 'price_gpu':args.price_gpu, 'price_disk':args.price_disk, 'price_inetu':args.price_inetu, 'price_inetd':args.price_inetd } );
+    r = requests.put(req_url, json = {'machine':args.id, 'price_gpu':args.price_gpu, 'price_disk':args.price_disk, 'price_inetu':args.price_inetu, 'price_inetd':args.price_inetd, 'min_chunk':args.min_chunk  } );
     
     if (r.status_code == 200) :
         #print(r.text);
@@ -550,7 +551,9 @@ def list__machine(args):
             price_gpu_   = str(args.price_gpu) if args.price_gpu is not None else "def";
             price_inetu_ = str(args.price_inetu);
             price_inetd_ = str(args.price_inetd);
-            print("offers created for machine {args.id},  @ ${price_gpu_}/gpu/day, ${price_inetu_}/GB up, ${price_inetd_}/GB down".format(**locals()));
+            min_chunk_ = str(args.min_chunk);
+
+            print("offers created for machine {args.id},  @ ${price_gpu_}/gpu/day, ${price_inetu_}/GB up, ${price_inetd_}/GB down, {min_chunk_}/min gpus".format(**locals()));
         else :
             print(rj["msg"]);
     else :
@@ -900,4 +903,3 @@ if __name__ == "__main__":
         main()
     except (KeyboardInterrupt, BrokenPipeError):
         pass
-
