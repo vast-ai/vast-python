9a42147089c72fb23700665d9003895ad7d4531f
Gregory Propf <gpropf@gmail.com>
2022-01-21 13:03:42 -0800

    Changes to README.md and watcher.sh.

--- watcher.sh
+++ watcher.sh
@@ -4,4 +4,8 @@
 # and then reruns the pdf generator script. If you have something like
 # evince running that reloads the PDF when it changes this can be very
 # convenient.
-watch -g ls -l *.py *.sh *.png; echo "Something changed. Sleeping 1..."; sleep 1; ./vast.py generate pdf-invoices; ./watcher.sh
+
+while true
+do
+  watch -g ls -l *.py *.sh *.png *.pdf; echo "Something changed. Sleeping 1..."; sleep 1; ./vast.py generate pdf-invoices
+done
